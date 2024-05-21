2f01c6b0d29324456e368ad3e70e45a2478213f9
Mohit <mohit.peak@gmail.com>
2024-05-22 00:50:50 +0530

    configured prisma

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express, { Express, Request, Response, Application } from "express";
 import cors from "cors";
 
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 const app: Application = express();
 app.use(cors());
 
 import { PORT } from "./config/server_config";
+import { log } from "console";
 
 app.use(express.json());
 
@@ -14,6 +18,15 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log("Server started on port 3000");
+  const user = await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "X9Xp7@example.com",
+      password: "123456",
+    },
+  });
+
+  console.log(user);
 });
